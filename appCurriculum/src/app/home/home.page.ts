af6a1ee8ffb8e91c368b12962a1ab8be80f4d498
Jorge Iza <izajorge7@gmail.com>
2020-01-05 12:46:36 -0500

    CV con fecha unixTime muestra la edad actual y hora del sistema

--- appCurriculum/src/app/home/home.page.ts
+++ appCurriculum/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  currentDate;
+  formattedDate;
+  formattedDateObj;
+  unixTime;
+  edadActual;
+  anio;
+  constructor(public navCtrl: NavController) {
+    this.currentDate = new Date()
+    this.getFormattedDate()
+    this.unixTime = this.currentDate.getTime()
+  }
+
+  
+  getFormattedDate(){
+    var dateObj = new Date()
+
+    var fNac = dateObj.getUTCFullYear() - 1993;
+    var year = dateObj.getFullYear().toString()
+    var month = dateObj.getMonth().toString()
+    var date = dateObj.getDate().toString()
+
+    var monthArray = ['Ene','Feb','Mar','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic']
+    this.formattedDate = year + '/' + monthArray[month] + '/' + date;
+    this.formattedDateObj = new Date(this.formattedDate);
+    this.edadActual = fNac;    
+  }
 
 }
